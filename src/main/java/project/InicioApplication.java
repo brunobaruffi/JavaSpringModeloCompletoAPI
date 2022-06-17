d27ea76ca86c7702139fb6225a59c582d187eec9
brunobaruffivia <bruno.baruffi@viavarejo.com.br>
2022-06-17 14:00:33 -0300

    [BUILD]Implementação inicial do Spring com a conf do banco de dados

---
+++ src/main/java/project/InicioApplication.java
@@ -0,0 +1,15 @@
+package project;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class InicioApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(InicioApplication.class,args);
+    }
+}
